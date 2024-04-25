20f56478cf0439f266b5c36bb118a60236de80e8
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-04-24 23:55:12 -0500

    some quotes and file for zoltar logic

---
+++ zoltar_quotes.py
@@ -0,0 +1,38 @@
+zoltar = [
+  "If you hear an onion ring, answer it.",
+  "Conscience is what hurts when everything else feels so good.",
+  "Be kind to unkind people – they need it the most.",
+  "Always borrow money from a pessimist; they don’t expect to be paid back!",
+  "There is nothing better than a friend, unless it is a friend with chocolate!",
+  "If Cinderella’s slipper fitted perfectly, why did it fall off?",
+  "With great power there comes an even greater electricity bill!",
+  "No matter how smart you are, you will never convince someone stupid that they are stupid.",
+  "A goal without a plan is just a wish!",
+  "Do not miss out on something that is great just because it could be difficult!",
+  "There is nothing better than a friend, unless it is a friend with chocolate!",
+  "If PLAN A fails all is not lost, there are 25 more letters in the alphabet!",
+  "Expecting the unexpected makes the unexpected expected!",
+  "Remember this, a true friend is the one that walks in when the rest of the world walks out.",
+  "All easy problems have already been solved!",
+  "Be kind to unkind people; they need it most.",
+  "A person that aims at nothing is sure to hit it.",
+  "If you always do what you have always done, you will always get what you always had.",
+  "A bus is a vehicle that runs twice as fast when you are wanting to catch it, than when you are riding in it.",
+  "There will come a point in your life when you realise who REALLY matters, who NEVER did, and who ALWAYS will.",
+  "Remember, cats have 32 muscles in each ear; this is to help them ignore you!",
+  "You do not realize what you have until it has gone; toilet paper is the best example!",
+  "Better late than never, but never late is better!",
+  "Those who live in the past limit their future!"
+]
+
+gifs_and_messages = [
+  ("/Users/gabrieltorres/Desktop/zoltar/exit1.gif", "Something Weird is Happening"),
+  ("/Users/gabrieltorres/Desktop/zoltar/exit2.gif", "Zoltar is upset"),
+  ("/Users/gabrieltorres/Desktop/zoltar/exit3.gif", "Zoltar is very upset"),
+  ("/Users/gabrieltorres/Desktop/zoltar/exit4.gif", "Zoltar is very very upset"),
+  ("/Users/gabrieltorres/Desktop/zoltar/exit5.gif", "Zoltar is very very very upset"),
+  ("/Users/gabrieltorres/Desktop/zoltar/dont-run.gif", "You cant run from Zoltar"),
+  ("/Users/gabrieltorres/Desktop/zoltar/transition1.gif", "Zoltar stares within your soul"),
+  ("/Users/gabrieltorres/Desktop/zoltar/transition2.gif", "Zoltar is not pleased"),
+  ("/Users/gabrieltorres/Desktop/zoltar/transition3.gif", "Zoltar sees within your soul")
+]
